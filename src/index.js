2ce67b104d8d3f10071777f23e0d56b8d52d898c
reboot420 <freedom88san@outlook.jp>
2025-04-18 23:11:47 +0900

    エントリーポイントファイルを追加

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AIDropshippingGuide from './AIDropshippingGuide';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AIDropshippingGuide />
+  </React.StrictMode>
+);
